659def85bf027fca34b6c50db77696607fb44bf0
arthur <arthur.mengual@gmail.com>
2021-12-09 11:53:10 -0300

    11h53

--- litreview/followers/views.py
+++ litreview/followers/views.py
@@ -1,16 +1,22 @@
 from django.contrib.auth import login
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from . import forms
+from authentication import models
 
 
 @login_required
 def followers(request):
-    form = forms.FollowersForm(instance=request.user)
+    actual_user = request.user
+    follows = actual_user.follows
     if request.method == 'POST':
-        form = forms.FollowersForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect('followers')
-    
-    return render(request, 'followers/followers.html', context={'form': form})
+        search = request.POST['username']
+        users = models.User.objects.all()
+        for user in users:
+            if user.username == search:
+                if user.username != request.user.username:
+                    actual_user.follows.add(user)
+                    actual_user.save()
+                    return redirect('followers')
+
+    return render(request, 'followers/followers.html', context={'follows': follows})
